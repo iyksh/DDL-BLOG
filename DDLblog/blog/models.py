d813ee79d50ecb9bc5efe339224c5647d816a526
iyksh <gssantoz2012@gmail.com>
2024-03-24 10:26:36 -0300

    First commit

---
+++ DDLblog/blog/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField()
+    intro = models.TextField()
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_at',]
+
+    def __str__(self):
+        return self.title
+    
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['created_at',]
+
+    def __str__(self):
+        return 'Comment {} by {}'.format(self.body, self.name)
---
+++ DDLblog/blog/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import get_object_or_404, render, redirect
+
+from .models import Post
+from .forms import CommentForm
+
+# Create your views here.
+
+def detail(request, slug):
+    
+    post = get_object_or_404(Post, slug=slug)
+    
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            
+            return redirect('post_detail', slug=post.slug)
+    
+    else:
+        form = CommentForm()
+        
+    return render(request, 'blog/detail.html', {'post': post, 'form': form})
