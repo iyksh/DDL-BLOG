c8a93e8ffcccfd3fb8ca2e26ee8b309d5c978584
iyksh <gssantoz2012@gmail.com>
2024-03-24 11:23:28 -0300

    Category field

--- DDLblog/blog/models.py
+++ DDLblog/blog/models.py
@@ -2,7 +2,20 @@ from django.db import models
 
 # Create your models here.
 
+class Category(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField()
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+        ordering = ['title',]
+
+    def __str__(self):
+        return self.title
+
 class Post(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='posts')
+    
     title = models.CharField(max_length=255)
     slug = models.SlugField()
     intro = models.TextField()
--- DDLblog/blog/urls.py
+++ DDLblog/blog/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from blog import views
 
 urlpatterns = [
-    path('<slug:slug>/', views.detail, name='post_detail')
+    path('<slug:category_slug>/<slug:slug>/', views.detail, name='post_detail'),
+    path('<slug:slug>/', views.category, name='category_detail')
     
     
 ]
